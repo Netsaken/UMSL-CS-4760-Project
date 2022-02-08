fd2ae0c6e6098cd93e56a5aa7a21bd4de5f056fb
Netsaken <robhar56@gmail.com>
2022-02-07 23:10:19 -0600

    Step 3 Commit

--- simplechain.c
+++ simplechain.c
@@ -1,16 +1,49 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int main (int argc, char *argv[]) {
    pid_t childpid = 0;
-   int i, n;
-   
-   if (argc != 2) {
-      fprintf(stderr, "Usage: %s processes\n",argv[0]);
-      return 1;
+   int i, option, index;
+   int n = 4;
+
+   while ((option = getopt (argc, argv, ":hp:csi")) != -1) {
+      switch (option)
+         {
+         case 'h':
+            printf("h is working!\n");
+            break;
+         case 'p':
+            n = atoi(optarg);
+            printf("p (nprocs goes here) is working!\n");
+            break;
+         case 'c':
+            printf("c (nchars goes here) is working!\n");
+            break;
+         case 's':
+            printf("s (sleeptime goes here) is working!\n");
+            break;
+         case 'i':
+            printf("i (niters goes here) is working!\n");
+            break;
+         case '?':
+            printf("Usage: %s -p [# of processes]\n",argv[0]);
+            return 1;
+         default:
+            break;
+      }
    }
-   n = atoi(argv[1]);
+
+   for (index = optind; index < argc; index++) {
+      printf ("Non-option argument %s\n", argv[index]);
+   }
+
+   //Reset processes to 4 if no number was passed through
+   if (n == 0) {
+      n = 4;
+   }
+
    for (i = 1; i < n; i++)
       if (childpid = fork())
          break;
